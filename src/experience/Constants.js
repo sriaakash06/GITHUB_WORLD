32af853da52f4c4d5a0827df7e2c1359565f8e32
sriaakash06 <asriaakash@gmail.com>
2026-04-20 01:17:47 +0530

    login

--- src/experience/Constants.js
+++ src/experience/Constants.js
@@ -1,25 +1,30 @@
 export const PALETTE = {
     grass: ['#7ba342', '#86af49', '#95c057'],
-    stone: '#8e8d8a',
+    stone: '#b0ada3',
     stoneDark: '#4e4e4e',
     wood: '#a68069',
-    woodDark: '#704c38',
+    building: '#f4f1ea', // Off-white for walls
+    trim: '#d9d2c5',
     roof: '#e2725b', // Terracotta
     window: '#fce38a',
     gold: '#ffd700',
     road: '#555555',
-    roadMark: '#999999'
+    black: '#111111'
 };
 
 export const LANG_COLORS = {
     'JavaScript': '#f7df1e',
     'Python': '#4584b6',
     'Go': '#00add8',
-    'Rust': '#dea584',
+    'Rust': '#ef642a',
     'TypeScript': '#3178c6',
     'C++': '#f34b7d',
     'Java': '#b07219',
     'HTML': '#e34c26',
     'CSS': '#563d7c',
     'Default': '#9e9e9e'
+};
+
+export const GET_STYLIZED_COLOR_FOR_LANG = (lang) => {
+    return LANG_COLORS[lang] || LANG_COLORS['Default'];
 };
